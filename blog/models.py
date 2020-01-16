de6f54940f90438df402edf947cadedfa4745423
Upendra Yadav <yadav.11@iitj.ac.in>
2020-01-16 11:02:51 +0530

    css

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.user')
+    title = models.CharField(max_length=264)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True,null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+    
+    def get_absolute_url(self):
+        return reverse('post_detail',kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments')
+    author = models.CharField(max_length=300)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=timezone.now())
+    approved_comments= models.BooleanField(default=False)
+    
+
+    def approve(self):
+        self.approved_comments=True
+        self.save()
+    
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
